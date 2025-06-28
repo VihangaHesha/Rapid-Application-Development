11ec241f5d8dc682b101964a169eb6be66f20ceb
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-06-28 15:08:19 +0530

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Express,Request,Response} from 'express';
+import {debuglog} from "node:util";
+
+//1.Initializing express app
+const app : Express = express();
+
+//2.Define the application port
+const port = 3000;
+
+//3.Define a simple HTTP Get Request
+// As the first parameter we have to give the url path
+app.get('/',(req : Request,res : Response) =>  {
+    res.send("HELLO!!!")
+});
+
+//4.Instruct the express app to listen on port 3000
+app.listen(port,()=>{
+    console.log(`Server is running at HTTP://localhost:${port}`)
+})
