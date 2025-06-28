2f3f29734bd6d846d6fe51ccca976db5fb4273e7
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-06-28 16:35:56 +0530

    Reconfigured the application into two files (index and app)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, {Express, Request, Response} from "express";
 
-//1.Initializing express app
 const app : Express = express();
 
-//3.Define a simple HTTP Get Request
-// As the first parameter we have to give the url path
+//2.1 Define Middlewares
+//Instruct to parse the request payload data to be converted to JSON format
+app.use(express.json())
+
 app.get('/',(req : Request,res : Response) =>  {
     res.send("HELLO WORLD!!!")
 });
