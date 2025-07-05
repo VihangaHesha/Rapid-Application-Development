12ec33ddb67e49b64906c48be28a22465c6c98ee
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2025-07-05 09:57:31 +0530

    Added .env file to the project to store the properties of the project and added "dotenv" dependency to the project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import app from "./app";
+import dotenv from "dotenv"
 
-const port = 3000;
+dotenv.config();//This load all the environment variables to this variable from the ".env" file
+
+// With "process.env.PORT" we can access the port that we defined in the .env
+//and if there is no .env file or it is undefined we can give it like bellow to take a default value
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`Server is running at HTTP://localhost:${port}`)
